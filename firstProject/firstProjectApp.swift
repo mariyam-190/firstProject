90ba97dee410d279ba488899e611f402414ad034
mariyam-190 <mariyam_190@yahoo.com>
2023-10-10 09:48:20 +0300

    Initial Commit

---
+++ firstProject/firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectApp.swift
+//  firstProject
+//
+//  Created by mariyam yasin on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
